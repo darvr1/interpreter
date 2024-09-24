2210d705cdf579f947dd26e696203d005c28258a
Dale Rivera <dalegeethoob@gmail.com>
2024-09-24 09:31:21 -0700

    implement StoreCode

---
+++ interpreter/bytecodes/StoreCode.java
@@ -0,0 +1,32 @@
+package interpreter.bytecodes;
+
+import interpreter.virtualmachine.VirtualMachine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreCode extends ByteCode{
+    private int offset;
+    private String id;
+    private int valuePopped;
+
+    @Override
+    public void execute(VirtualMachine vm) {
+        valuePopped = vm.store(offset);
+    }
+
+    @Override
+    public void init(List<String> args) {
+        this.offset = Integer.parseInt(args.get(1));
+        if (args.size() > 2) this.id = args.get(2);
+    }
+
+    @Override
+    public String toString() {
+        String result = "STORE " + this.offset;
+        if (this.id != null)  {
+            result += " this.id" + "\t\t" + this.id + "=" + valuePopped;
+        }
+        return result;
+    }
+}
