c66cbb9f38b0b10ca60e317dd5465a37cbd02ded
Dale Rivera <dalegeethoob@gmail.com>
2024-09-23 21:56:05 -0700

    implement loadCodes()

--- interpreter/loaders/ByteCodeLoader.java
+++ interpreter/loaders/ByteCodeLoader.java
@@ -1,6 +1,13 @@
 package interpreter.loaders;
 
+import interpreter.bytecodes.ByteCode;
 import interpreter.loaders.Program;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public final class ByteCodeLoader {
     private String codSourceFileName;
@@ -21,6 +28,30 @@ public final class ByteCodeLoader {
      * loadCodes fails.
      */
     public Program loadCodes() throws InvalidProgramException {
-       return null;
+        Program program = new Program();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.codSourceFileName))) {
+            while (reader.ready()) {
+                String[] items = reader.readLine().split("\\s+");
+
+                // Load class blueprint
+                String className = CodeTable.getClassName(items[0]);
+                ByteCode bc = (ByteCode) Class
+                        .forName("interpreter.bytecodes." + className)
+                        .getDeclaredConstructor()
+                        .newInstance();
+
+//                System.out.println(bc.getClass().getName()); // FIXME
+
+                // Exclude first element since it cannot be an argument.
+                bc.init(Arrays.asList(Arrays.copyOfRange(items, 1, items.length)));
+                program.addCode(bc);
+            }
+        } catch(IOException | ClassNotFoundException | InvocationTargetException | InstantiationException |
+                IllegalAccessException | NoSuchMethodException ex) {
+            throw new InvalidProgramException(ex);
+        }
+
+        return program;
     }
 }
