a02ecbd2dd71461e0e3dd01868e646fe6662e045
Dale Rivera <dalegeethoob@gmail.com>
2024-09-24 12:51:44 -0700

    implement ArgsCode

---
+++ interpreter/bytecodes/ArgsCode.java
@@ -0,0 +1,24 @@
+package interpreter.bytecodes;
+
+import interpreter.virtualmachine.VirtualMachine;
+
+import java.util.List;
+
+public class ArgsCode extends ByteCode{
+    private int numOfArgs;
+
+    @Override
+    public void execute(VirtualMachine vm) {
+        vm.newFrameAt(numOfArgs);
+    }
+
+    @Override
+    public void init(List<String> args) {
+        this.numOfArgs = Integer.parseInt(args.get(1));
+    }
+
+    @Override
+    public String toString() {
+        return "ARGS " + numOfArgs;
+    }
+}
