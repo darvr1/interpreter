aa8ae77c793a22edf33eefcfaa986361359aa56f
Dale Rivera <dalegeethoob@gmail.com>
2024-09-25 22:48:13 -0700

    implement FalseBranchCode

---
+++ interpreter/bytecodes/FalseBranchCode.java
@@ -0,0 +1,35 @@
+package interpreter.bytecodes;
+
+import interpreter.virtualmachine.VirtualMachine;
+
+import java.util.List;
+
+public class FalseBranchCode extends ByteCodeJump {
+    private String label;
+    private int address;
+
+    @Override
+    public void execute(VirtualMachine vm) {
+        if (vm.pop() == 0) vm.setProgramCounter(address);
+    }
+
+    @Override
+    public void init(List<String> args) {
+        this.label = args.get(1);
+    }
+
+    @Override
+    public String toString() {
+        return "FALSEBRANCH " + this.label;
+    }
+
+    @Override
+    public void setAddress(int address) {
+        this.address = address;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.label;
+    }
+}
