af69a563b5aab6955c3fd4a167e8c0c329d927d0
Dale Rivera <dalegeethoob@gmail.com>
2024-09-25 23:56:41 -0700

    implement CallCode

---
+++ interpreter/bytecodes/CallCode.java
@@ -0,0 +1,48 @@
+package interpreter.bytecodes;
+
+import interpreter.virtualmachine.VirtualMachine;
+
+import java.util.List;
+
+public class CallCode extends ByteCodeJump {
+    private String label;
+    private String args;
+    private int address;
+
+    @Override
+    public void execute(VirtualMachine vm) {
+        args = vm.getArgs();
+        vm.storeReturnAddress();
+        vm.setProgramCounter(address);
+    }
+
+    @Override
+    public void init(List<String> args) {
+        this.label = args.get(1);
+    }
+
+    @Override
+    public String toString() {
+        String result = "CALL " + this.label;
+
+        // Get base id
+        String baseId = this.label;
+        int a = this.label.indexOf("<");
+        if (a != -1) {
+            baseId = this.label.substring(0, this.label.indexOf("<"));
+        }
+
+        result += "\t" + baseId + "(" + args + ")";
+        return result;
+    }
+
+    @Override
+    public void setAddress(int address) {
+        this.address = address;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.label;
+    }
+}
